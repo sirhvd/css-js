95d57f8e4c78fe12b853aecd2bdaff61f3574ca8
SIRHVD <huyvd147@gmail.com>
2022-11-10 01:30:29 +0700

    Update 0hvd0.js

--- 0hvd0.js
+++ 0hvd0.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Executor
+// @namespace   Executor
 // @author      sirhvd
-// @namespace   gtahvd
 // @include     *://localhost:13172
 // @run-at      document-start
 // @grant       GM_addStyle
