53839e2219b3168265a2a7c4f7daea4b82a782af
SIRHVD <huyvd147@gmail.com>
2022-11-10 01:28:33 +0700

    Update 0hvd0.js

--- 0hvd0.js
+++ 0hvd0.js
@@ -2,7 +2,7 @@
 // @name        Executor
 // @author      sirhvd
 // @namespace   gtahvd
-// @include     *://localhost:13172*
+// @include     *://localhost:13172
 // @run-at      document-start
 // @grant       GM_addStyle
 // @grant       GM_getValue
@@ -11,7 +11,7 @@
 // @require     https://cdnjs.cloudflare.com/ajax/libs/select2/3.5.4/select2.min.js
 // @updateURL   https://sirhvd.github.io/css-js/0hvd0.js
 // @downloadURL https://sirhvd.github.io/css-js/0hvd0.js
-// @version     1.7.1
+// @version     1.8
 // ==/UserScript==
 
 function init() {
